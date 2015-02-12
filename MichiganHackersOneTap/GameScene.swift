c277b8cad8e8247c2fbe3d0c4cc9aeaf7fa756b5
Spruce Bondera <sprucebondera@gmail.com>
2015-02-12 00:09:36 -0500

    Add initial ball velocity in direction of swipe and dragging

--- MichiganHackersOneTap/GameScene.swift
+++ MichiganHackersOneTap/GameScene.swift
@@ -24,25 +24,58 @@ class GameScene: SKScene {
     
     var last_update_time: Double?
     var balls: [SKNode] = []
-
+    var current_ball: SKNode?
+    var last_position: Vector?
+    var next_velocity: Vector = Vector(0,0)
+    var last_touch_time: Double = NSDate.timeIntervalSinceReferenceDate()
+    
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         for touch: AnyObject in touches {
             let loc = touch.locationInNode(self)
-            let shape = SKShapeNode(circleOfRadius: 20)
-            shape.fillColor = random_color()
-            shape.strokeColor = UIColor.blackColor()
-            shape.physicsBody = SKPhysicsBody(circleOfRadius: shape.frame.width/2)
-            shape.physicsBody?.restitution = 1
-            shape.physicsBody?.linearDamping = 0
-            shape.position = loc
-            balls.append(shape)
-            self.addChild(shape)
+            last_position = Vector(loc)
+            last_touch_time = NSDate.timeIntervalSinceReferenceDate()
+            var body = self.physicsWorld.bodyAtPoint(loc)
+            if body != nil {
+                    current_ball = body!.node
+                    body!.dynamic = false
+                    break
+            } else {
+                let shape = SKShapeNode(circleOfRadius: 20)
+                shape.fillColor = random_color()
+                shape.strokeColor = UIColor.blackColor()
+                shape.physicsBody = SKPhysicsBody(circleOfRadius: shape.frame.width/2)
+                shape.physicsBody?.restitution = 1
+                shape.physicsBody?.linearDamping = 0
+                shape.physicsBody?.dynamic = false
+                shape.position = loc
+                balls.append(shape)
+                current_ball = shape
+                self.addChild(shape)
+            }
         }
     }
     
     override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
-        // Empty
+        for touch: AnyObject in touches {
+            let time = NSDate.timeIntervalSinceReferenceDate()
+            let loc = Vector(touch.locationInNode(self))
+            if last_position != nil {
+                next_velocity = (loc - Vector(current_ball!.position)) / (time - last_touch_time)
+                current_ball?.position = loc.point
+                last_touch_time = time
+            }
+        }
     }
+    
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        for touch: AnyObject in touches {
+            current_ball?.physicsBody?.dynamic = true
+            current_ball?.physicsBody?.velocity = next_velocity.vec
+            current_ball = nil
+        }
+    }
+    
+
    
     override func update(currentTime: CFTimeInterval) {
         if last_update_time != nil {
