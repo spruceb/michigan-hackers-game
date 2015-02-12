ad430a2b68548c173c2edfa6e89efaa200a02215
Spruce Bondera <sprucebondera@gmail.com>
2015-02-11 23:22:40 -0500

    Add ball-creation code for physics testing

--- MichiganHackersOneTap/GameScene.swift
+++ MichiganHackersOneTap/GameScene.swift
@@ -8,31 +8,36 @@
 
 import SpriteKit
 
+func random_color() -> UIColor {
+    let colors = [UIColor.redColor(), UIColor.greenColor(), UIColor.blackColor(), UIColor.blueColor(), UIColor.yellowColor(), UIColor.purpleColor()]
+    let random_index = Int(arc4random_uniform(UInt32(colors.count)))
+    return colors[random_index]
+}
+
+
 class GameScene: SKScene {
-    var circle: SKShapeNode?
     
     override func didMoveToView(view: SKView) {
-        self.backgroundColor = UIColor.greenColor()
+        self.backgroundColor = SKColor.whiteColor()
         self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
     }
     
     var last_update_time: Double?
-    
+    var balls: [SKNode] = []
 
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-//        for touch: AnyObject in touches {
-//            if circle == nil {
-//                let loc = touch.locationInNode(self)
-//                let shape = SKShapeNode(circleOfRadius: 40)
-//                shape.fillColor = UIColor.blackColor()
-//                shape.strokeColor = UIColor.blackColor()
-//                shape.position = loc
-//                circle = shape
-//                self.addChild(shape)
-//            } else {
-//                
-//            }
-//        }
+        for touch: AnyObject in touches {
+            let loc = touch.locationInNode(self)
+            let shape = SKShapeNode(circleOfRadius: 20)
+            shape.fillColor = random_color()
+            shape.strokeColor = UIColor.blackColor()
+            shape.physicsBody = SKPhysicsBody(circleOfRadius: shape.frame.width/2)
+            shape.physicsBody?.restitution = 1
+            shape.physicsBody?.linearDamping = 0
+            shape.position = loc
+            balls.append(shape)
+            self.addChild(shape)
+        }
     }
     
     override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
