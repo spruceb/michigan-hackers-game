b46bbea18ab8137941e5491a5c16acde8ae9ee08
Spruce Bondera <sprucebondera@gmail.com>
2015-02-16 00:18:24 -0500

    Add scrolling stars background

--- MichiganHackersOneTap/GameScene.swift
+++ MichiganHackersOneTap/GameScene.swift
@@ -8,10 +8,9 @@
 
 import SpriteKit
 
-
 class GameScene: SKScene {
     var last_update_time: Double?
-    var player = SKShapeNode(circleOfRadius: 20)
+    var player = SKSpriteNode(imageNamed: "player")
     var last_touch_position: Vector?
     var touch_offset: Vector {
         get {
@@ -23,20 +22,36 @@ class GameScene: SKScene {
         }
     }
     
+    var backgrounds: [SKSpriteNode] = []
+    let backgroundScrollRate = 0.9
     
-    override func didMoveToView(view: SKView) {
-        self.backgroundColor = SKColor.whiteColor()
-        self.physicsWorld.gravity = CGVector(dx: 0,dy: 0)
-        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
-        self.player.position = Vector(Double(view.frame.width)/2.0,
-                                      Double(player.frame.height)/2.0 +
-                                      Double(view.frame.height) * 0.1 ).point
-        self.player.fillColor = SKColor.blackColor()
-        self.player.strokeColor = SKColor.blackColor()
+    func init_player() {
+        self.player.position = Vector(Double(self.view!.frame.width)/2.0,
+            Double(player.frame.height)/2.0 +
+                Double(self.view!.frame.height) * 0.1 ).point
         self.player.physicsBody = SKPhysicsBody(circleOfRadius: player.frame.width/2)
         self.player.physicsBody?.dynamic = false
+        self.player.zPosition = 10
         self.addChild(player)
-        
+    }
+    
+    func init_background() {
+        var background_images = [SKSpriteNode(imageNamed: "background_1"), SKSpriteNode(imageNamed: "background_2")]
+        for i in 0..<2 {
+            var background = background_images[i]
+            background.size = self.view!.frame.size
+            background.anchorPoint = CGPoint(x: 0, y: 0)
+            background.position = CGPoint(x: 0, y: CGFloat(i) * background.size.height - CGFloat(i))
+            self.addChild(background)
+            backgrounds.append(background)
+        }
+    }
+    
+    override func didMoveToView(view: SKView) {
+        self.backgroundColor = SKColor.blackColor()
+        self.physicsWorld.gravity = CGVector(dx: 0,dy: 0)
+        init_background()
+        init_player()
         self.view?.multipleTouchEnabled
     }
     
@@ -54,7 +69,6 @@ class GameScene: SKScene {
     
     override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
         for touch: AnyObject in touches {
-            println("newtouch")
             let touch_pos = Vector(touch.locationInNode(self))
             let player_pos = Vector(self.player.position)
             let new_pos = touch_pos + touch_offset
@@ -76,13 +90,26 @@ class GameScene: SKScene {
         }
     }
     
+    func background_update(delta_t: Double) {
+        let screen_height = self.view!.frame.height
+        for i in 0..<2 {
+            let image = backgrounds[i]
+            if image.position.y < -screen_height {
+                image.position.y = backgrounds[1-i].position.y + image.size.height
+            }
+            image.position.y -= CGFloat(backgroundScrollRate * Double(screen_height) * delta_t)
+        }
+    }
+    
     override func update(currentTime: CFTimeInterval) {
-        if last_update_time != nil {
+        if last_update_time == nil {
             // Game loop
             last_update_time = currentTime
-        } else {
-            last_update_time = currentTime
         }
+        let delta_t = currentTime - last_update_time!
+        background_update(delta_t)
+        
+        last_update_time = currentTime
     }
     
 }
